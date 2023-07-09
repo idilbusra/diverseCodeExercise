5b5dac06ec35597ae5711ae852cfc96d21f63ff8
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-09 11:21:58 +0300

    StringAndDouble Class has been created and added first question.

---
+++ src/main/java/variables/StringAndDoubleQ1.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class StringAndDoubleQ1 {
+    /*
+    Create a String and two double variables.
+    Print the sum and the multiplication of the double variables with the String on the console.
+     */
+
+
+}
