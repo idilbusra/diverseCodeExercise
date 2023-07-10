8a32565600b85d41dfa7baab10247b357d7ee83c
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-10 23:22:59 +0300

    BodyMassIndexQ5 Class created and fifth question added.

---
+++ src/main/java/variables/BodyMassIndexQ5.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class BodyMassIndexQ5 {
+
+     /*
+    Type a code to find simple body mass index.
+    NOTE: BMI(Body Mass Index) = weight *10000 / (height*height)
+     */
+
+}
