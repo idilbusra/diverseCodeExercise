8736639e8599f650748d0a05a0d139c0ca54c39d
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-10 23:07:13 +0300

    BooleanQ4 Class has been created and added fourth question and solved.

---
+++ src/main/java/variables/BooleanQ4.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class BooleanQ4 {
+    /*
+     Create two boolean variables whose values are different and print their values in the same
+    line with a space between two consecutive values.
+
+     */
+
+    public static void main(String[] args) {
+
+        boolean isRetired = true;
+        boolean isStudent = false;
+        System.out.println(isRetired + " " + isStudent);
+
+
+    }
+
+}
