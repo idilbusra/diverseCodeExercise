4a2b89ed2e58bff3acdf8c105eef7d3066b53de1
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-10 22:59:17 +0300

    ByteShortIntLongQ3 Class has been created and added third question and solved.

---
+++ src/main/java/variables/ByteShortIntLongQ3.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class ByteShortIntLongQ3 {
+
+    /*
+    Create a byte variable, a short variable, an integer variable and a long variable for any 4 items. Print the
+multiplication of the values on the console with a label.
+     */
+    public static void main(String[] args) {
+        byte footNumber = 38;
+        short populationOfSchool = 23546;
+        int populationOfVillage = 3675337;
+        long populationOfChina = 14257000000000L;
+
+        System.out.println("Result : " + (footNumber*populationOfSchool*populationOfVillage*populationOfChina));
+
+
+    }
+}
