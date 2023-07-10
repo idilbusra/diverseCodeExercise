52f218da34c7451f445df35de47d6f99d2bd1a62
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-10 22:40:54 +0300

    Float Class has been created and added second question and solved.

---
+++ src/main/java/variables/FloatQ2.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class FloatQ2 {
+    /*
+    Create float variables for the prices of any 3 items. Print the sum of the prices on the
+console with a label.
+     */
+    public static void main(String[] args) {
+
+        float bookPrice = 39.99F;
+        float penPrice = 19.99F;
+        float shoesPrice = 659.99F;
+
+        System.out.println("Total Price: "+ (bookPrice+penPrice+shoesPrice));
+
+
+
+    }
+}
