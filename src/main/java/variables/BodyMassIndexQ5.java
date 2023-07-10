916162525d30a8c7723443d942e1c3c6ca88a305
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-10 23:57:26 +0300

    Fifth question solved.

--- src/main/java/variables/BodyMassIndexQ5.java
+++ src/main/java/variables/BodyMassIndexQ5.java
@@ -7,4 +7,12 @@ public class BodyMassIndexQ5 {
     NOTE: BMI(Body Mass Index) = weight *10000 / (height*height)
      */
 
+    public static void main(String[] args) {
+        int weight = 65;
+        int height =171;
+
+        double bodyMassIndex = weight *10000 / (height*height);
+
+        System.out.println(bodyMassIndex);
+    }
 }
