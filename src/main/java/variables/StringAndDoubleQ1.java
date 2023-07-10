3c226a6ddb9809eecf7c4d4cc97c454f6b1bbb5a
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-10 22:24:53 +0300

    First question solved.

--- src/main/java/variables/StringAndDoubleQ1.java
+++ src/main/java/variables/StringAndDoubleQ1.java
@@ -6,5 +6,17 @@ public class StringAndDoubleQ1 {
     Print the sum and the multiplication of the double variables with the String on the console.
      */
 
+    public static void main(String[] args) {
+
+        String s = "Result : ";
+        double d1 = 56131638.516;
+        double d2 = 5526.25;
+
+        System.out.println(s + (d1+d2) );
+        System.out.println(s+ d1*d2 );
+
+
+
+    }
 
 }
