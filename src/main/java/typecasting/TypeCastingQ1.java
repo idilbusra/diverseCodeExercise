dfebbec025dd383ab0502937fe9e887dc68e6d5f
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-11 23:55:02 +0300

    First question solved.

--- src/main/java/typecasting/TypeCastingQ1.java
+++ src/main/java/typecasting/TypeCastingQ1.java
@@ -4,5 +4,11 @@ public class TypeCastingQ1 {
     /*
     Create a byte variable and convert it to an int variable.
      */
+    public static void main(String[] args) {
+
+        byte age = 15;
+        int newAge = age;
+
+    }
 
 }
