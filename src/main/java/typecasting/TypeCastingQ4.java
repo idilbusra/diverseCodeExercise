e86c975d0f718b989db2fba360005ba6d631011e
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-12 00:00:52 +0300

    Fourth question added

---
+++ src/main/java/typecasting/TypeCastingQ4.java
@@ -0,0 +1,8 @@
+package typecasting;
+
+public class TypeCastingQ4 {
+    /*
+     Create a byte variable and convert it to a double variable, then print the value of the double
+variable on the console. Be careful about the output, it will be a decimal number.
+     */
+}
