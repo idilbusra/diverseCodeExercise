7b5a5d0a200f91ca2b6379cc81dde94cfbec288a
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-13 00:00:22 +0300

    TypeCastinQ3 has been solved.

--- src/main/java/typecasting/TypeCastingQ3.java
+++ src/main/java/typecasting/TypeCastingQ3.java
@@ -4,4 +4,8 @@ public class TypeCastingQ3 {
     /*
     Create a long variable and convert it to an int variable.
      */
+    public static void main(String[] args) {
+        long population = 626517865315654l;
+        int newPopulation= (int)population;
+    }
 }
