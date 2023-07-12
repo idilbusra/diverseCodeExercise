e66bf7bce1a01b6bf878bd9173d03a1c7f272887
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-12 23:58:29 +0300

    TypeCastingQ2 has been solved.

--- src/main/java/typecasting/TypeCastingQ2.java
+++ src/main/java/typecasting/TypeCastingQ2.java
@@ -4,5 +4,13 @@ public class TypeCastingQ2 {
     /*
     Create a double variable and convert it to a float variable
      */
+    public static void main(String[] args) {
+        double weight = 89.645;
+
+        float newWeight = (float)weight;
+
+        System.out.println(weight);
+        System.out.println(newWeight);
+    }
 
 }
--- src/main/java/typecasting/TypeCastingQ4.java
+++ src/main/java/typecasting/TypeCastingQ4.java
@@ -5,4 +5,6 @@ public class TypeCastingQ4 {
      Create a byte variable and convert it to a double variable, then print the value of the double
 variable on the console. Be careful about the output, it will be a decimal number.
      */
+
+
 }
