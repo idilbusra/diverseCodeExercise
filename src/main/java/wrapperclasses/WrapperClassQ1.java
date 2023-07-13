c928a4f394beffc5d25d516c00fb0237223ef64a
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-13 23:48:45 +0300

    First question has solved.

--- src/main/java/wrapperclasses/WrapperClassQ1.java
+++ src/main/java/wrapperclasses/WrapperClassQ1.java
@@ -5,7 +5,15 @@ public class WrapperClassQ1 {
     Type a code to find the sum of the minimum value of the int and the maximum value of
 double data types.
      */
+    public static void main(String[] args) {
 
+        int min = Integer.MIN_VALUE;
+        double max = Double.MAX_VALUE;
+
+        System.out.println(min + max);
+
+
+    }
 
 
 }
