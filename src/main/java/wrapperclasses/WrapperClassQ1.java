1a842ba52092893fd10b5b5e3e62e6e1b874143e
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-13 23:47:11 +0300

    WrapperClassQ1 has been created and first question added.

---
+++ src/main/java/wrapperclasses/WrapperClassQ1.java
@@ -0,0 +1,11 @@
+package wrapperclasses;
+
+public class WrapperClassQ1 {
+    /*
+    Type a code to find the sum of the minimum value of the int and the maximum value of
+double data types.
+     */
+
+
+
+}
