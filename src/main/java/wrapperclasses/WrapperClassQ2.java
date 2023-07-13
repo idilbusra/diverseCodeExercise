f078d3e114dd305cbf5dba4305940d5987f31ebc
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-13 23:57:05 +0300

    WrapperClassQ2 has added.

---
+++ src/main/java/wrapperclasses/WrapperClassQ2.java
@@ -0,0 +1,8 @@
+package wrapperclasses;
+
+public class WrapperClassQ2 {
+    /*
+    Type a code to convert “250” String to byte and to convert “2000” String to short then print
+the difference on the console.
+     */
+}
