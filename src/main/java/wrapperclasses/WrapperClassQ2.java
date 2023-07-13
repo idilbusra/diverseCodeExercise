cd9dd05348dfbfee845783f56b4321f6e7672961
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-13 23:59:18 +0300

    WrapperClassQ2 has solved.

--- src/main/java/wrapperclasses/WrapperClassQ2.java
+++ src/main/java/wrapperclasses/WrapperClassQ2.java
@@ -5,4 +5,10 @@ public class WrapperClassQ2 {
     Type a code to convert “250” String to byte and to convert “2000” String to short then print
 the difference on the console.
      */
+    public static void main(String[] args) {
+        byte num1 = Byte.valueOf("250");
+        short num2 = Short.valueOf("2000");
+        System.out.println(num2 - num1);
+
+    }
 }
