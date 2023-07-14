80f7c5262f363ba65b3338f66187782a5be7dbb3
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-14 22:40:48 +0300

    WrapperClassQ3 has added.

---
+++ src/main/java/wrapperclasses/WrapperClassQ3.java
@@ -0,0 +1,9 @@
+package wrapperclasses;
+
+public class WrapperClassQ3 {
+    /*
+    Type a code to find the multiply of the minimum value of the int and the maximum value of
+byte data types.
+     */
+
+}
