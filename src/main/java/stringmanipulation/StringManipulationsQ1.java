d9b64c4491fbc7a0ead8eeb6cf3ff7734ad5da6e
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-14 23:30:32 +0300

    First  question has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ1.java
+++ src/main/java/stringmanipulation/StringManipulationsQ1.java
@@ -5,5 +5,11 @@ public class StringManipulationsQ1 {
     Create a String variable for person names which have just a single word.
 Print the person name with the initial is in lowercase and all the other characters
      */
+    public static void main(String[] args) {
+        String nameOfPerson = " bUSRA ";
+        String updatedNameOfPerson = nameOfPerson.trim().toUpperCase();
+        updatedNameOfPerson = updatedNameOfPerson.substring(0,1).toLowerCase() + updatedNameOfPerson.substring(0,1);
+        System.out.println(updatedNameOfPerson);
 
+    }
 }
