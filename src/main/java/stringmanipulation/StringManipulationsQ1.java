6a5f5f2514515dbffd74d91d603a8ba9ca3d5a82
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-14 23:24:20 +0300

    StringManipulationsQ1 has created and first question added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ1.java
@@ -0,0 +1,9 @@
+package stringmanipulation;
+
+public class StringManipulationsQ1 {
+    /*
+    Create a String variable for person names which have just a single word.
+Print the person name with the initial is in lowercase and all the other characters
+     */
+
+}
