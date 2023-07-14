6fed460fd0883efeda58c2e33d683299b3c31fd9
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-14 22:42:40 +0300

    Third question has solved.

--- src/main/java/wrapperclasses/WrapperClassQ3.java
+++ src/main/java/wrapperclasses/WrapperClassQ3.java
@@ -5,5 +5,15 @@ public class WrapperClassQ3 {
     Type a code to find the multiply of the minimum value of the int and the maximum value of
 byte data types.
      */
+    public static void main(String[] args) {
+        int min = Integer.MIN_VALUE;
+        byte max = Byte.MAX_VALUE;
 
+        System.out.println(min * max);
+
+
+
+
+
+    }
 }
