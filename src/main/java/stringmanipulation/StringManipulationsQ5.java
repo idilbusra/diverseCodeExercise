8eeeee6182829a1dc31b4b98de856b3b7f3910cf
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-15 23:53:37 +0300

    Fifth has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ5.java
+++ src/main/java/stringmanipulation/StringManipulationsQ5.java
@@ -5,4 +5,10 @@ public class StringManipulationsQ5 {
      Create a String variable and print just the last non-space character on the console for any
 String.
      */
+    public static void main(String[] args) {
+        String str = " Istanbul ";
+        int indexOfLastNonSpace = str.trim().length()-1;
+        String lastNonSpaceChar = str.substring(indexOfLastNonSpace, indexOfLastNonSpace+1);
+        System.out.println("The non-space last character: " + lastNonSpaceChar);
+    }
 }
