e3af289921c8ca5cb7b23f97553c04a351dd688f
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-15 23:22:08 +0300

    StringManipulationsQ5 has created and fifth question has added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ5.java
@@ -0,0 +1,8 @@
+package stringmanipulation;
+
+public class StringManipulationsQ5 {
+    /*
+     Create a String variable and print just the last non-space character on the console for any
+String.
+     */
+}
