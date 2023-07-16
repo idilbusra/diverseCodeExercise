9d3264a014c420b29f72cfe6fd50a16e845a2d0f
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:07:44 +0300

    StringManipulationsQ8 class has created and quesiton has added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ8.java
@@ -0,0 +1,8 @@
+package stringmanipulation;
+
+public class StringManipulationsQ8 {
+    /*
+    Create a String variable and print all characters except the last character on the console in
+uppercases.
+     */
+}
