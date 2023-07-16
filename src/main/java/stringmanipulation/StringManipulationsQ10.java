53d054cf7466f6444e26e5db54f36020154ec954
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:19:42 +0300

    StringManipulationsQ10 classes has created and question has added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ10.java
@@ -0,0 +1,8 @@
+package stringmanipulation;
+
+public class StringManipulationsQ10 {
+    /*
+    Type code to check if a String has just a single space character any position in the middle.
+
+     */
+}
