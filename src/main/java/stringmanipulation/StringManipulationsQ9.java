15bf950a4921f2196f646c072ad07ed12ea8c391
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:13:57 +0300

    StringManipulationsQ9 has created and question has added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ9.java
@@ -0,0 +1,8 @@
+package stringmanipulation;
+
+public class StringManipulationsQ9 {
+    /*
+     Create a String variable and print all characters except the first character and the last
+character on the console in uppercases.
+     */
+}
