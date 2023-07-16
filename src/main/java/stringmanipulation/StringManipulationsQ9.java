0816837ae73fc878f502de693120624efd7b45e5
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:15:59 +0300

    Nineth question has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ9.java
+++ src/main/java/stringmanipulation/StringManipulationsQ9.java
@@ -5,4 +5,14 @@ public class StringManipulationsQ9 {
      Create a String variable and print all characters except the first character and the last
 character on the console in uppercases.
      */
+    public static void main(String[] args) {
+        String str = "Java";
+        Integer indexOfLastCharacter = str.length()-1;
+        String allCharactersExceptLastCharactersInUpperCase = str.substring(1, indexOfLastCharacter).toUpperCase();
+        System.out.println("All characters except the first and the last character: " + allCharactersExceptLastCharactersInUpperCase);
+
+
+
+
+    }
 }
