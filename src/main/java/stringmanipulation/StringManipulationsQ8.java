08941f1c2f73b86c6a6258f5c6691664f71e25c5
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:11:26 +0300

    Eighth question has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ8.java
+++ src/main/java/stringmanipulation/StringManipulationsQ8.java
@@ -5,4 +5,14 @@ public class StringManipulationsQ8 {
     Create a String variable and print all characters except the last character on the console in
 uppercases.
      */
+
+    public static void main(String[] args) {
+        String str = "Busra";
+        Integer indexOfLastCharacter = str.length()-1;
+        String allCharactersExceptLastCharacter = str.substring(0, indexOfLastCharacter).toUpperCase();
+        System.out.println("All characters except the last character: " + allCharactersExceptLastCharacter);
+
+
+
+    }
 }
