9787d6a580c649f7bdda3fd5bc593f8be3106e00
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:27:17 +0300

    StringManipulations11 class has created and question has added.

---
+++ src/main/java/stringmanipulation/StringManipulations11.java
@@ -0,0 +1,8 @@
+package stringmanipulation;
+
+public class StringManipulations11 {
+    /*
+    Type code to check if a String does not have any space character at the beginning and at
+the end.
+     */
+}
