66350823e08a1c5ef5a3f15a22f8e9d69a777dd4
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-16 23:25:00 +0300

    Tent question has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ10.java
+++ src/main/java/stringmanipulation/StringManipulationsQ10.java
@@ -5,4 +5,13 @@ public class StringManipulationsQ10 {
     Type code to check if a String has just a single space character any position in the middle.
 
      */
+
+    public static void main(String[] args) {
+        String str = "Tom Hanks";
+        String trimMiddle = str.trim();
+        String allExceptedSpace = trimMiddle.replaceAll("\\s", "");
+        Boolean isSingleSpaceInTheMiddle = trimMiddle.length() - allExceptedSpace.length()==1;
+        System.out.println("There is a space in the middle: " + isSingleSpaceInTheMiddle);
+
+    }
 }
