b4939150a0fee407dd235e60e6aeb361f8b73cba
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-19 22:34:21 +0300

    StringManipulationsQ12 class has created and question has added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ13.java
@@ -0,0 +1,9 @@
+package stringmanipulation;
+
+public class StringManipulationsQ13 {
+    /*
+    Type code to check if a password is valid or not for the following conditions;
+    Password must have at least 9 characters different from space character.
+    Password must have at least 3 symbol.
+     */
+}
