0b2f1150f6f4443aa61e1ac0e257c3050b30bd62
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-19 22:31:42 +0300

    Twelfth question has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ12.java
+++ src/main/java/stringmanipulation/StringManipulationsQ12.java
@@ -3,8 +3,15 @@ package stringmanipulation;
 public class StringManipulationsQ12 {
     public static void main(String[] args) {
         /*
-         Type code to check if a String has an uppercase initial and dot at the end.
+         Type code to check if a String has an uppercase initial and question mark at the end.
          */
 
+        String str = "Tom Hanks.";
+        char firstCharacter = str.charAt(0);
+        char lastCharacter = str.charAt(str.length()-1);
+        Boolean isTheFirstCharacterUpper = firstCharacter >='A' && lastCharacter <='Z';
+        Boolean isTheLastCharacterQuestion = lastCharacter =='?';
+        Boolean isFirstUpperAndLastQuestion = isTheFirstCharacterUpper && isTheLastCharacterQuestion;
+        System.out.println("Is the first char upper and the last char dot? " + isFirstUpperAndLastQuestion);
     }
 }
