259de3e366267bdc6d94fc0609409fb0439c204c
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-19 22:17:41 +0300

    StringManipulationsQ12 classes has created and question has added.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ12.java
@@ -0,0 +1,10 @@
+package stringmanipulation;
+
+public class StringManipulationsQ12 {
+    public static void main(String[] args) {
+        /*
+         Type code to check if a String has an uppercase initial and dot at the end.
+         */
+
+    }
+}
