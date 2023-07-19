e2f6c449e6c4a6497a4e1d982e57d8f00c5b97ed
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-19 22:42:17 +0300

    StringManipulationsQ14 class has created and quesiton add.

---
+++ src/main/java/stringmanipulation/StringManipulationsQ14.java
@@ -0,0 +1,11 @@
+package stringmanipulation;
+
+public class StringManipulationsQ14 {
+    public static void main(String[] args) {
+ /*
+  Type code to get initials of the first name and the last name of a given name. Middle name
+        is out of scope
+  */
+
+    }
+}
