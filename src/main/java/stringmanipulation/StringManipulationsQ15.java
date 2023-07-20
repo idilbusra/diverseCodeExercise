2ddfc66a9ed573fa273dcb4d61d1c8a3df796bdc
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-20 23:52:21 +0300

    Fifth question has solved.

--- src/main/java/stringmanipulation/StringManipulationsQ15.java
+++ src/main/java/stringmanipulation/StringManipulationsQ15.java
@@ -4,5 +4,19 @@ public class StringManipulationsQ15 {
     /*
     Type a code to find the number of punctuation marks used in a String.
      */
+    public static void main(String[] args) {
+
+        String str = "Wow! Busra is 28 years old. ";
+        int totalNumberOfCharacters = str.length();
+        int charactersDifferentFromPunctuationMarks = str.replaceAll("\\p{Punct}", "").length();
+        int numOfPunctuationMarks = totalNumberOfCharacters - charactersDifferentFromPunctuationMarks;
+        System.out.println(numOfPunctuationMarks);
+
+
+
+
+
+    }
+
 
 }
