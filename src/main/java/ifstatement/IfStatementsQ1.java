3cfa2dc024b8544d2087ac44bd0efff9a01ae3e8
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-20 23:57:44 +0300

    IfStatementsQ1 has solved.

--- src/main/java/ifstatement/IfStatementsQ1.java
+++ src/main/java/ifstatement/IfStatementsQ1.java
@@ -1,4 +1,28 @@
 package ifstatement;
 
 public class IfStatementsQ1 {
+    /*
+    Type code to print
+    "Winter" for December, January, February
+     */
+    public static void main(String[] args) {
+
+        String monthName = "December";
+        monthName = monthName.toLowerCase();
+        if(monthName.equals("december") || monthName.equals("january") || monthName.equals("february")){
+            System.out.println("Winter");
+        }else if(monthName.equals("march") || monthName.equals("april") || monthName.equals("may")){
+            System.out.println("Spring");
+        }else if(monthName.equals("june") || monthName.equals("july") || monthName.equals("august")){
+            System.out.println("Summer");
+        }else if(monthName.equals("september") || monthName.equals("october") || monthName.equals("november")){
+            System.out.println("Fall");
+        }else{
+            System.out.println("Invalid month name");
+        }
+
+
+
+
+    }
 }
