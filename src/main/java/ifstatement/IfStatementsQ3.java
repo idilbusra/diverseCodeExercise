11a63de28780153da4886d2d2b982a88be572b37
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-21 22:05:05 +0300

    Thirth question has solved.

--- src/main/java/ifstatement/IfStatementsQ3.java
+++ src/main/java/ifstatement/IfStatementsQ3.java
@@ -9,4 +9,18 @@ public class IfStatementsQ3 {
  character in any position
  c) "Invalid Password" if a and b conditions are not satisfied
      */
+    public static void main(String[] args) {
+        String password = "q1w2e3";
+        if(password.replaceAll("\\S", "").length()>0){
+            System.out.println("Do not use space character in password");
+        }else if(password.replaceAll("\\s", "").length()>=6){
+            System.out.println("Valid Password");
+        }else{
+            System.out.println("Invalid Password");
+        }
+
+
+
+
+    }
 }
