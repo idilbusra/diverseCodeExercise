b96bfad3959594e1117967c1f30f9d60af1275e0
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-21 23:02:28 +0300

    IfStatementsQ5 class has created and question has added.

---
+++ src/main/java/ifstatement/IfStatementsQ5.java
@@ -0,0 +1,12 @@
+package ifstatement;
+
+public class IfStatementsQ5 {
+    /*
+     Type code to print the messages about the Body Mass Index(BMI) for the given BMI values
+ Invalid BMI value < 0
+ Underweight = <18.5
+ Normal weight = 18.5–24.9
+ Overweight = 25–29.9
+ Obesity = BMI of 30 or greater
+     */
+}
