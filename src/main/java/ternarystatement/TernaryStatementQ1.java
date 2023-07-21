567d8e8901e9101eb110b7dfbcb6fa4ab2389292
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-21 23:10:02 +0300

    TernaryStatementQ1 class has created and question has added.

---
+++ src/main/java/ternarystatement/TernaryStatementQ1.java
@@ -0,0 +1,9 @@
+package ternarystatement;
+
+public class TernaryStatementQ1 {
+    /*
+    Use ternary to print "Valid Password" if the password has at least 5 characters different
+from space characters. "Invalid Password" if the password has less than 5 characters
+different from space character.
+     */
+}
