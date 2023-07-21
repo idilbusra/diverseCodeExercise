6e2d9e27bda5409dd3ce5cf54cc0151cdf8791cd
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-21 22:02:16 +0300

    IfStatementsQ3 class has created and question has added.

---
+++ src/main/java/ifstatement/IfStatementsQ3.java
@@ -0,0 +1,12 @@
+package ifstatement;
+
+public class IfStatementsQ3 {
+    /*
+     Type code to print
+ a) "Valid Password" if the password has at least 6 characters different from space
+ character
+ b) "Do not use space character in password" if the password has any space
+ character in any position
+ c) "Invalid Password" if a and b conditions are not satisfied
+     */
+}
