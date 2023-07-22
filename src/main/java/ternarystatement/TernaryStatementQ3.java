b8dfd018196eee0480e299c7e62aaa9b6b8beee2
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-22 23:57:52 +0300

    Third question has solved.

--- src/main/java/ternarystatement/TernaryStatementQ3.java
+++ src/main/java/ternarystatement/TernaryStatementQ3.java
@@ -7,4 +7,9 @@ public class TernaryStatementQ3 {
  If it does not have more than 5 characters initial should be 'I'
  Solve the task by using nested-ternary
      */
+    public static void main(String[] args) {
+        String password = "12345687";
+        String checkPassword = password.length() > 5 ? password.startsWith("B") ? "Valid" : "Invalid" : password.startsWith("I") ? "Valid" : "Invalid";
+        System.out.println(checkPassword);
+    }
 }
