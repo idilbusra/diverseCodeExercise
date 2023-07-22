6f1b05d8a1c2d2a418e56f9ab74a5fe935db7933
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-22 23:47:55 +0300

    TernaryStatementQ2 class has created and has added.

---
+++ src/main/java/ternarystatement/TernaryStatementQ2.java
@@ -0,0 +1,10 @@
+package ternarystatement;
+
+public class TernaryStatementQ2 {
+    /*
+    Use ternary to print
+a) "Isosceles Triangle" if two sides of a triangle are equal in length.
+b) "Equilateral Triangle" if all sides of a triangle are equal in length.
+c) "Neither isosceles, nor equilateral" if a and b conditions are not content.
+     */
+}
