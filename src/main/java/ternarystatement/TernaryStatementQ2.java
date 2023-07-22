e2b4b9466c5612ba0e0c82637c05aed8f71a5bdc
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-22 23:51:58 +0300

    Second question has solved.

--- src/main/java/ternarystatement/TernaryStatementQ2.java
+++ src/main/java/ternarystatement/TernaryStatementQ2.java
@@ -7,4 +7,14 @@ a) "Isosceles Triangle" if two sides of a triangle are equal in length.
 b) "Equilateral Triangle" if all sides of a triangle are equal in length.
 c) "Neither isosceles, nor equilateral" if a and b conditions are not content.
      */
+    public static void main(String[] args) {
+        int a = 4, b = 4, c = 3;
+        String typeOfTriangle = a==b && b==c ?
+                "Equilateral Triangle" :
+                (a==b && b!=c || a==c && b!=c || b==c && a!=c ?
+                        "Isosceles Triangle" :
+                        "It's not both an isosceles triangle and a scalene triangle.");
+        System.out.println(typeOfTriangle);
+
+    }
 }
