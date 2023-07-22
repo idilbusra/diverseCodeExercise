d2a4c93a9489c516ec97cc6d18493e7d2aca4631
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-22 23:55:21 +0300

    TernaryStatementQ3 class has created and question has added.

---
+++ src/main/java/ternarystatement/TernaryStatementQ3.java
@@ -0,0 +1,10 @@
+package ternarystatement;
+
+public class TernaryStatementQ3 {
+    /*
+    Type code to check the password
+ If it has more than 5 characters, initial should be 'B'
+ If it does not have more than 5 characters initial should be 'I'
+ Solve the task by using nested-ternary
+     */
+}
