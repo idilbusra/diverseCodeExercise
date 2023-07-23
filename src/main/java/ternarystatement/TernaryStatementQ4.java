30572577d01154cb3940e87ea091274335bafdb3
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-23 23:22:39 +0300

    Fourth question has solved.

--- src/main/java/ternarystatement/TernaryStatementQ4.java
+++ src/main/java/ternarystatement/TernaryStatementQ4.java
@@ -4,4 +4,13 @@ public class TernaryStatementQ4 {
     /*
     Write a program to print the minimum of 2 integers on the console by using ternary.
      */
+    public static void main(String[] args) {
+        int firstNumber = 25;
+        int secondNumber = 29;
+
+        int result = firstNumber<secondNumber? firstNumber : secondNumber;
+
+        System.out.println(result);
+    }
+
 }
