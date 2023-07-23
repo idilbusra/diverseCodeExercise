8dbd71835bd68b66add5ce11ecc458ed668bc8b7
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-23 23:24:54 +0300

    TernaryStatementQ5 class has created and question has added.

---
+++ src/main/java/ternarystatement/TernaryStatementQ5.java
@@ -0,0 +1,8 @@
+package ternarystatement;
+
+public class TernaryStatementQ5 {
+    /*
+    If the number is positive print ‘Negative’, otherwise print ‘Not Negative’ on the console by
+using ternary.
+     */
+}
