c17d0a86f5cc3bb7496b2979f0e27623512e5ef0
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-23 23:32:48 +0300

    TernaryStatementQ6 class created and question has added.

---
+++ src/main/java/ternarystatement/TernaryStatementQ6.java
@@ -0,0 +1,8 @@
+package ternarystatement;
+
+public class TernaryStatementQ6 {
+    /*
+    If the number has 4 digits, the output will be “This number has 4 digits.” Otherwise, the
+output will be “This number has no 4 digits.”
+     */
+}
