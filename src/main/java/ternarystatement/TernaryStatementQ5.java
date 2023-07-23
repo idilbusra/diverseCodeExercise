88283e9bb327450a3ceed818929b5dd60083743a
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-23 23:26:47 +0300

    Fifth question has solved.

--- src/main/java/ternarystatement/TernaryStatementQ5.java
+++ src/main/java/ternarystatement/TernaryStatementQ5.java
@@ -5,4 +5,9 @@ public class TernaryStatementQ5 {
     If the number is positive print ‘Negative’, otherwise print ‘Not Negative’ on the console by
 using ternary.
      */
+    public static void main(String[] args) {
+        int number = 39;
+        String result = number>0 ? "Negative" : "Not Negative";
+        System.out.println(result);
+    }
 }
