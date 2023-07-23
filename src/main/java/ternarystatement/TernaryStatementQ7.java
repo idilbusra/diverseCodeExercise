87d20bbe386122e03402ad4f156eacd952003cd4
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-23 23:39:20 +0300

    Seventh question has solved.

--- src/main/java/ternarystatement/TernaryStatementQ7.java
+++ src/main/java/ternarystatement/TernaryStatementQ7.java
@@ -4,4 +4,9 @@ public class TernaryStatementQ7 {
     /*
     Print 'Odd' for odd integers, print ‘Even’ for even integers by using ternary.
      */
+    public static void main(String[] args) {
+        int number = 39;
+        String result = number %2==0 ? "Even" : "Odd";
+        System.out.println(result);
+    }
 }
