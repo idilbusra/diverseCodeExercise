efd441fff020f929070c0f7d66b775111d0b590a
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-23 23:35:01 +0300

    Sixth question has solved.

--- src/main/java/ternarystatement/TernaryStatementQ6.java
+++ src/main/java/ternarystatement/TernaryStatementQ6.java
@@ -5,4 +5,13 @@ public class TernaryStatementQ6 {
     If the number has 4 digits, the output will be “This number has 4 digits.” Otherwise, the
 output will be “This number has no 4 digits.”
      */
+    public static void main(String[] args) {
+        int number = 3639;
+        String str = (number>999 && number<10000) || (number>-10000 && number<-999) ? "Has 4 digits" : "Has no 4 digits";
+        System.out.println(str);
+
+
+
+
+    }
 }
