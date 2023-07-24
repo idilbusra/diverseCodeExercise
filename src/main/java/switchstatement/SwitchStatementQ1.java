15b12be9f86a5acbdc963a7726c626a75377b90f
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-24 23:11:13 +0300

    SwitchStatementQ1 has created and has question has added.

---
+++ src/main/java/switchstatement/SwitchStatementQ1.java
@@ -0,0 +1,8 @@
+package switchstatement;
+
+public class SwitchStatementQ1 {
+    /*
+    Use a switch statement to print the name of the month when you enter the number of the
+month. If user enters invalid month number your code should print "Invalid number".
+     */
+}
