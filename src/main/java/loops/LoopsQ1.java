da1856b1a9e533a68eb30f8e9e009891163216a2
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-24 23:55:23 +0300

    First question has solved.

--- src/main/java/loops/LoopsQ1.java
+++ src/main/java/loops/LoopsQ1.java
@@ -5,4 +5,22 @@ public class LoopsQ1 {
     Type all integers which are divisible by 6 and divisible by 9 from 390 to 18 in the same line
 with a space between two consecutive integers.
      */
+    public static void main(String[] args) {
+        String str = "";
+        Integer number =390;
+        while(number>17){
+            if(number%6==0 && number%9==0){
+                str = str + number + " ";
+            }
+            number--;
+        }
+        System.out.println(str);
+
+
+
+
+
+
+    }
+
 }
