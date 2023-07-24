acd8ece34f6c37e145ae9c235dc08b5e97669e40
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-24 23:53:04 +0300

    LoopsQ1 class has created and question has added.

---
+++ src/main/java/loops/LoopsQ1.java
@@ -0,0 +1,8 @@
+package loops;
+
+public class LoopsQ1 {
+    /*
+    Type all integers which are divisible by 6 and divisible by 9 from 390 to 18 in the same line
+with a space between two consecutive integers.
+     */
+}
