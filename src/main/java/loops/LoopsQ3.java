96b18e57c865298e4ed273136533ec71cfadeba9
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-25 21:50:42 +0300

    LoopsQ3 class has created and questin has added.

---
+++ src/main/java/loops/LoopsQ3.java
@@ -0,0 +1,8 @@
+package loops;
+
+public class LoopsQ3 {
+    /*
+    Type code to check a String is palindrome or not.
+    If a String is the same with its reverse then it is called palindrome.
+     */
+}
