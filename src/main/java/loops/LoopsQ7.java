f8b7fe719f764f142e3ada6dd517fe1bd2b47d42
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-26 23:04:56 +0300

    Seventh question has solved.

--- src/main/java/loops/LoopsQ7.java
+++ src/main/java/loops/LoopsQ7.java
@@ -4,4 +4,16 @@ public class LoopsQ7 {
     /*
     Type code to print all lowercase characters in a String with *
      */
+    public static void main(String[] args) {
+        String str = "busra hello world";
+        str = str.replaceAll("[^a-z]", "");
+        String str1 = "";
+        Integer i=0;
+        do{
+            String str3 = str.substring(i,i+1);
+            str1 = str1 + str3 + '*';
+            i++;
+        }while(i< str.length());
+        System.out.println(str1);
+    }
 }
