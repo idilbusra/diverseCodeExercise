dfcce5d1360ff6e07c8c7cd89c77a5efab670aac
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-26 22:43:08 +0300

    Sixth question has solved.

--- src/main/java/loops/LoopsQ6.java
+++ src/main/java/loops/LoopsQ6.java
@@ -4,4 +4,16 @@ public class LoopsQ6 {
     /*
     Write the code to print even integers from 50 to 2 on the same line by adding "+" between the two consecutive ones.
      */
+    public static void main(String[] args) {
+
+        String str = "";
+        for(Integer i=50; i>2; i--){
+            if(i%2==0){
+                str = str + i + " ";
+            }
+        }
+        System.out.println(str);
+
+
+    }
 }
