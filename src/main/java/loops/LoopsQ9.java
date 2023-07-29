e638ff8f59deb0ba48f010c5a769ef3955f2c4cd
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-29 23:11:07 +0300

    Nineth question has solved.

--- src/main/java/loops/LoopsQ9.java
+++ src/main/java/loops/LoopsQ9.java
@@ -4,4 +4,11 @@ public class LoopsQ9 {
     /*
     Type code to find the sum of the integers from 9 to 53
      */
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=9; i<54; i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
 }
