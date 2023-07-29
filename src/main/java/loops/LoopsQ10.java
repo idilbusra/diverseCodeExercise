e5f944722ebf88c42bcba3acec501f399c918288
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-29 23:28:50 +0300

    Tenth question has solved.

--- src/main/java/loops/LoopsQ10.java
+++ src/main/java/loops/LoopsQ10.java
@@ -4,4 +4,11 @@ public class LoopsQ10 {
     /*
     Type code to find the multiplication of the integers from 9 to 53
      */
+    public static void main(String[] args) {
+        int multiply = 1;
+        for(int i=9; i<54; i++){
+            multiply = multiply *i;
+        }
+        System.out.println(multiply);
+    }
 }
