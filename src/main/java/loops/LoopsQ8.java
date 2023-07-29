4bfbac716df6f313fcc4a6a1171aaa0e8acdc729
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-29 23:04:52 +0300

    Eighth question has solved.

--- src/main/java/loops/LoopsQ8.java
+++ src/main/java/loops/LoopsQ8.java
@@ -4,4 +4,16 @@ public class LoopsQ8 {
     /*
     Type code to print numbers just in the decimal part of the given decimal number with a space.
      */
+    public static void main(String[] args) {
+        double number = 19.3256;
+        String str = String.valueOf(number);
+        int idx = str.indexOf(".");
+        String partOfDecimal = str.substring(idx + 1);
+        String s1 = "";
+        for(Integer i=0; i<partOfDecimal.length(); i++){
+            String s2 = partOfDecimal.substring(i,i+1);
+            s1 = s1 + " " + s2;
+        }
+        System.out.println(s1);
+    }
 }
