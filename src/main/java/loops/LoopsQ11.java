7d331b601ca7a44375a52084c88784f681e9411b
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-30 22:34:18 +0300

    Eleventh question has solved.

--- src/main/java/loops/LoopsQ11.java
+++ src/main/java/loops/LoopsQ11.java
@@ -4,4 +4,11 @@ public class LoopsQ11 {
     /*
     Type code to print characters from 'a' to 'x' on the console by using for loop
      */
+    public static void main(String[] args) {
+        int result = 0;
+        for(char i = 'a'; i<='x'; i++){
+            result = result + i;
+        }
+        System.out.println(result);
+    }
 }
