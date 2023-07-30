ca332293fefcb3ac5fed6ce9583a65347c183760
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-30 22:40:24 +0300

    Twelve question has solved.

--- src/main/java/loops/LoopsQ12.java
+++ src/main/java/loops/LoopsQ12.java
@@ -4,4 +4,13 @@ public class LoopsQ12{
     /*
     Type code to print characters from 'a' to 'S' on the console by using while loop.
      */
+    public static void main(String[] args) {
+        char character = 'S';
+        String result = "";
+        while(character <='a'){
+            result = result + character;
+            character++;
+        }
+        System.out.println(result);
+    }
 }
