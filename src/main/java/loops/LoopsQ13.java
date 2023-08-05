f24c8f5896223036b6f4a249a8cbca6f1799ead3
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-05 23:13:00 +0300

    Thirteen question has solved.

--- src/main/java/loops/LoopsQ13.java
+++ src/main/java/loops/LoopsQ13.java
@@ -4,4 +4,14 @@ public class LoopsQ13 {
     /*
     Type a program to print the integers from 1 to 1000 without using any loop
      */
+    public static void main(String[] args) {
+        printNumbers(1000);
+    }
+    static void printNumbers(int number) {
+        if(number > 0) {
+            printNumbers(number - 1);
+            System.out.print(number + " ");
+        }
+        return;
+    }
 }
