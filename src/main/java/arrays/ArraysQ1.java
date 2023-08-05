6e3c979052f21ef8fa90474be3712fca328df63b
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-05 23:24:01 +0300

    First question has solved.

--- src/main/java/arrays/ArraysQ1.java
+++ src/main/java/arrays/ArraysQ1.java
@@ -4,4 +4,16 @@ public class ArraysQ1 {
     /*
     Get the initials of the elements in a String array, if the String ends with 'b' or 'i'
      */
+    public static void main(String[] args) {
+        String[] words = new String[]{"busra", "idil.i", "busraidil", "Busra.i", "Idil"};
+        String initials = "";
+        for(String w : words){
+            if(w.endsWith("b") || w.endsWith("i")){
+                initials = initials + w.substring(0,1);
+            }
+        }
+        System.out.println(initials);
+
+
+    }
 }
