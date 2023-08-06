70872e044d72e6de8e61c9a5d5d81c1705acc551
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-06 23:25:39 +0300

    Second question has solved.

--- src/main/java/arrays/ArraysQ2.java
+++ src/main/java/arrays/ArraysQ2.java
@@ -4,4 +4,12 @@ public class ArraysQ2 {
     /*
     Type code to find sum of the number of characters of array elements.
      */
+    public static void main(String[] args) {
+        String[] arr = new String[]{"busra", "idil", "coding", "hello", "world"};
+        Integer sum = 0;
+        for(String w : arr){
+            sum = sum + w.length();
+        }
+        System.out.println(sum);
+    }
 }
