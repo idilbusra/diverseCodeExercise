8457cedc4bf6da19320be9d89fdf29c639d313f4
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-06 23:52:47 +0300

    Second question has solved.

--- src/main/java/dates/dateClassQ2.java
+++ src/main/java/dates/dateClassQ2.java
@@ -1,9 +1,21 @@
 package dates;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class dateClassQ2 {
     /*
     Type the code that finds out how many months Busra lived.
 Date of birth of Busra is 19 of October 1995
      */
+
+    public static void main(String[] args) {
+        LocalDate birthdayOfBusra = LocalDate.of(1995, 10, 19);
+        LocalDate presentDate = LocalDate.now();
+        long numberMonthsOfBusraLived = ChronoUnit.MONTHS.between(birthdayOfBusra, presentDate);
+        System.out.println("The number of months Busra lived is " + numberMonthsOfBusraLived);
+
+
+    }
 
 }
