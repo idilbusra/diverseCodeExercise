d4931659ac81cffd2870290185c028cef290b3cc
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-06 23:39:19 +0300

    First question has solved.

--- src/main/java/dates/dateClass.java
+++ src/main/java/dates/dateClass.java
@@ -1,8 +1,22 @@
 package dates;
 
-public class dateclass {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class dateClass {
     /*
     Type the code that finds out how many days Busra lived.
 Date of birth of Busra is 19 of October 1995
      */
+    public static void main(String[] args) {
+        LocalDate birthdayOfBusra = LocalDate.of(1995, 10, 19);
+        LocalDate presentDate = LocalDate.now();
+        long numberDaysOfBusraLived = ChronoUnit.DAYS.between(birthdayOfBusra, presentDate);
+        System.out.println("The number days of Busra lived is " + numberDaysOfBusraLived);
+
+
+
+
+
+    }
 }
